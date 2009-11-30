b22be72e12eb61d3e533266721112aab37d3f37b
stone <stones.gao@gmail.com>
2009-12-01 03:17:52 +0800

    use updated version of ActiveDirect

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,6 @@ class Category < ActiveRecord::Base
   acts_as_nested_set
   has_many :videos
   acts_as_direct :foo => 0, :bar => {:len => 1, :formHandler => true}
-  puts 'called...'
   
   def self.foo
   
